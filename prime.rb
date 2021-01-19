d5a764b5f09c6424230ab73f1e5451fd2e4e1e99
pbenton6 <patrick.benton6396@gmail.com>
2021-01-19 16:26:50 -0600

    commit tree 32a009a20bfc2062802897548cd7ae18ee22382a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+    if integer <= 1
+        return false
+    elsif integer <= 3
+        return true
+    else (2..integer/2).none? do |num|
+        integer % num == 0
+    end
+end
+end
+
+
+
